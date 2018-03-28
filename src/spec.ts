cacf845503dacc041840eed38c9267b35214643b
Mychal Thompson <mychalrthompson@gmail.com>
2018-03-28 00:40:37 -0400

    Added xit and fit to spec

--- src/spec.ts
+++ src/spec.ts
@@ -2,6 +2,9 @@ export interface Spec<T> {
     beforeEach(callback: Spec.Callback<T>): void;
     afterEach(callback: Spec.Callback<T>): void;
     it(description: string, callback: Spec.Callback<T>): void;
+
+    xit?(description: string, callback: Spec.Callback<T>): void;
+    fit?(description: string, callback: Spec.Callback<T>): void;
 }
 
 export namespace Spec {
@@ -10,11 +13,12 @@ export namespace Spec {
 
     // Creates a helper object that provides type-safe wrappers of BDD functions for a test spec
     export function create<T>(): Spec<T> {
-        return {
+        return Object.assign({
             beforeEach: (callback: Callback<T>) => beforeEach(inject<T>(callback)),
             afterEach: (callback: Callback<T>) => afterEach(inject<T>(callback)),
-            it: (description: string, callback: Callback<T>) => it(description, inject<T>(callback)),
-        };
+            it: (description: string, callback: Callback<T>) => it(description, inject<T>(callback))
+        }, fit ? { fit: (description: string, callback: Callback<T>) => fit(description, inject<T>(callback)) } : {},
+           xit ? { xit: (description: string, callback: Callback<T>) => xit(description, inject<T>(callback)) } : {});
     }
 
     // Creates a wrapper around BDD functions that allows injection of type-safe properties for the test spec
